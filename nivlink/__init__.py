19c6c7ef70498099ed1b2ea2b894c0aed401ad0a
Angela Radulescu <angelar@princeton.edu>
2018-08-30 13:55:20 -0400

    added mapping function

--- nivlink/__init__.py
+++ nivlink/__init__.py
@@ -5,3 +5,4 @@ __version__ = '0.1'
 from .epoching import epoching_fht
 from .preprocessing import (align_to_aoi, compute_fixations)
 from .screen import (ScreenInfo)
+from .mapping import (map_to_feats)
