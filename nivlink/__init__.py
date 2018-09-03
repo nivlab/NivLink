7e921ddd1482fe47ef968fdf056a57cc5efb1c9a
Angela Radulescu <angelar@princeton.edu>
2018-09-03 15:09:57 -0400

    removed mapping function from init

--- nivlink/__init__.py
+++ nivlink/__init__.py
@@ -5,4 +5,3 @@ __version__ = '0.1'
 from .epoching import epoching_fht
 from .preprocessing import (align_to_aoi, compute_fixations)
 from .screen import (ScreenInfo)
-from .mapping import (map_to_feats)
