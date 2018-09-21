5f485d892c040bafb2cb21ac8a82f0363016d1a4
Sam Zorowitz <szorowi1@gmail.com>
2018-09-21 00:59:07 -0400

    version update

--- nivlink/__init__.py
+++ nivlink/__init__.py
@@ -1,6 +1,6 @@
 """Niv Lab software for preprocessing eyelink eyetracking data."""
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 from .preprocessing import (align_to_aoi, compute_fixations)
 from .screen import (ScreenInfo)
