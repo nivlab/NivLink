a6ca94ad4352c4816b1f6124f98db8625439e1c0
Angela Radulescu <angelar@princeton.edu>
2018-10-21 17:51:27 -0400

    remove old epoching function

--- nivlink/__init__.py
+++ nivlink/__init__.py
@@ -5,4 +5,4 @@ __version__ = '0.1'
 from .preprocessing import (align_to_aoi, compute_fixations)
 from .screen import (ScreenInfo)
 from .fht import epoching_fht
-from .moat import epoching_moat, epoching_moat2, set_screen_moat, set_custom_centers, make_screen_idx, remap_aois, plot_moat_heatmaps
+from .moat import epoching_moat, set_screen_moat, set_custom_centers, make_screen_idx, remap_aois, plot_moat_heatmaps
--- nivlink/moat.py
+++ nivlink/moat.py
@@ -1,62 +1,6 @@
 import numpy as np
 
-def epoching_moat(raw, info, events, template='Start of Run%s'):
-    """Epoch the raw eyetracking data.
-    
-    Parameters
-    ----------
-    raw : array, shape (n_times, 3)
-        Raw eyetracking data. The first column contains the event messages.
-        The second and third columns contain the eye positions in the x- and
-        y-dimensions, respectively.
-    info : instance of `ScreenInfo`
-        Eyetracking acquisition information.
-    events : array, shape (n_trials, 3)
-        Events data. Each row represents a trial. The first column
-        denotes the block. The second column denotes the trial onset
-        relative to the block. The third column denotes the trial length.
-    template : str
-        Template for block start note as found in the EyeLink output file.
-    
-    Returns
-    -------
-    epochs : array, shape (n_trials, n_times, 2)
-        Epoched eyetracking timeseries data. Last dimension is the
-        spatial dimension of the eyetracker (xdim, ydim).
-      
-    Notes
-    -----
-    Designed for MOAT dataset collected by Daniel Bennett & Angela Radulescu. 
-    Key assumption is that each "Start of Run message" is aligned to the 
-    first stimulus onset within that run/block. We only look at last 4 blocks.
-    """
-
-    ## Recode block in events file (these are only the test blocks).
-    events[:,0] = events[:,0] + int(3)
-
-    ## Define elapsed time relative to eyetracking.
-    times = np.arange(0, raw.shape[0] / info.sfreq, 1 / info.sfreq)
-    
-    ## Define start of blocks relative to elapsed time.
-    blocks = np.unique(events[:,0]).astype(int) 
-    indices = [(raw[:,0]==template %i).argmax() for i in blocks]
-    block_starts = np.zeros_like(times)
-    block_starts[indices] = blocks
-    
-    events = events.copy()
-    for block, offset in zip(blocks, times[block_starts.nonzero()]):
-        events[events[:,0]==block, 1] += offset
-
-    ## Redefine trial onset as index corresponding to elapsed time.
-    onsets = np.array([np.argmax(times > t) for t in events[:,1]])
-    offsets = (onsets + info.sfreq * events[:,-1]).astype(int)
-    
-    ## Epoching.
-    epochs = np.array([raw[i:j+1, 1:] for i,j in np.column_stack([onsets,offsets])])
-
-    return epochs.astype(float)
-
-def epoching_moat2(messages, data, info, events):
+def epoching_moat(messages, data, info, events):
     """Epoch the raw eyetracking data. This function has ragged array support.
        Can deprecate once we switch to NivLink 2.0.
     
